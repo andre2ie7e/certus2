a60f2ef55109f1db727b33dbc2a514f2283a8935
Andres Flores <andresfg1499@gmail.com>
2025-05-09 08:28:33 -0500

    mi primer commit

---
+++ models/Alumno.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const alumnoSchema = new mongoose.Schema({
+    nombre: {type: String, required: true},
+    apellido: {type: String, required: true},
+    edad: {type: Number, required: true},
+    genero: {type: String, required: true},
+    carrera: {type: String, required: true},
+    comentarios: {type: String, required: true},
+    }, {
+        timestamps: true
+});
+module.exports = mongoose.model('Alumno', alumnoSchema, 'alumnos');
